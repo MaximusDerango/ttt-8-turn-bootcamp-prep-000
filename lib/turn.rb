fc8409e20ee6709abce0eb2af48d6f679817de90
Maxim Dalton <maximcdalton@gmail.com>
2018-06-03 05:33:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,1 +1,15 @@
-def display_board
+def display_board(board)
+end
+
+def input_to_index(pos)
+  pos.to_i - 1
+end
+
+def valid_move?(board, pos)
+  pos.between?(0, 8) and [" ", "", nil].include?(board[pos])
+end
+
+def move(board, pos, marker="X")
+end
+
+def turn()
