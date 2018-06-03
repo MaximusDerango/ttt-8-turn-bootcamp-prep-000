5db075ea6d6e5fa72da773a43478d77343f49278
Maxim Dalton <maximcdalton@gmail.com>
2018-06-03 05:42:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,9 @@
 def display_board(board)
-  row_1 = (0, 3).collect{|x| " #{board[x]} "}.join("|")
+  row_1 = (0, 2).collect{|x| " #{board[x]} "}.join("|")
+  row_2 = (3, 6).collect{|x| " #{board[x]} "}.join("|")
+  row_3 = (7, 9).collect{|x| " #{board[x]} "}.join("|")
   div = "-"*11
+  [row_1, row_2, row_3].join(div)
 end
 
 def input_to_index(pos)
